8341c4af11162189e0d542a6abb973ae7ed557bc
AndreyIslentiev <andrey.islentiev@incsub.com>
2022-03-04 15:38:45 +1000

    Fixed: doesn't copy attachment while importing .ics

--- includes/ical/class-calendar-plus-ical-sync.php
+++ includes/ical/class-calendar-plus-ical-sync.php
@@ -47,9 +47,9 @@ class Calendar_Plus_iCal_Sync {
 		$this->locations = calendarp_get_locations();
 
 		$args = wp_parse_args( $args, [
-			'author'   => 0,
-			'category' => 0,
-			'status'   => 'publish',
+            'author'   => 0,
+            'category' => 0,
+            'status'   => 'publish',
 		] );
 
 		$this->event_author = $args['author'] ? $args['author'] : get_current_user_id();
@@ -86,13 +86,14 @@ class Calendar_Plus_iCal_Sync {
 	protected function sync_event( $event_data ) {
 
 		$event_data = wp_parse_args( $event_data, [
-			'uid'          => '',
-			'post_title'   => '',
-			'post_content' => '',
-			'post_status'  => $this->default_status,
-			'location'     => '',
-			'last_updated' => '',
-			'categories'   => [],
+            'uid'          => '',
+            'post_title'   => '',
+            'post_content' => '',
+            'post_status'  => $this->default_status,
+            'location'     => '',
+            'last_updated' => '',
+            'categories'   => [],
+            'attachment'   => [],
 		] );
 
 		if ( 'publish' === $event_data['post_status'] ) {
@@ -102,10 +103,10 @@ class Calendar_Plus_iCal_Sync {
 		$event = calendarp_get_event_by_uid( $event_data['uid'] );
 
 		$post_args = array(
-			'post_type'    => 'calendar_event',
-			'post_status'  => $event_data['post_status'],
-			'post_title'   => $event_data['post_title'],
-			'post_content' => $event_data['post_content'],
+            'post_type'    => 'calendar_event',
+            'post_status'  => $event_data['post_status'],
+            'post_title'   => $event_data['post_title'],
+            'post_content' => $event_data['post_content'],
 		);
 
 		if ( $event ) {
@@ -151,10 +152,10 @@ class Calendar_Plus_iCal_Sync {
 		}
 
 		$rules = array(
-			'from_date'  => date( 'Y-m-d', $event_data['from'] ),
-			'until_date' => date( 'Y-m-d', $event_data['to'] ),
-			'from_time'  => date( 'H:i', $event_data['from'] ),
-			'until_time' => date( 'H:i', $event_data['to'] ),
+            'from_date'  => date( 'Y-m-d', $event_data['from'] ),
+            'until_date' => date( 'Y-m-d', $event_data['to'] ),
+            'from_time'  => date( 'H:i', $event_data['from'] ),
+            'until_time' => date( 'H:i', $event_data['to'] ),
 		);
 
 		$type = $rules['from_date'] === $rules['until_date'] ? '' : 'datespan';
@@ -168,7 +169,70 @@ class Calendar_Plus_iCal_Sync {
 			update_post_meta( $post_id, '_ical_last_updated', $event_data['last_updated'] );
 		}
 
+		if( $event_data['attachment'] ) {
+
+			$current_attachment = wp_get_attachment_url( $post_id );
+			if ( $current_attachment !== $event_data['attachment'] ) {
+
+				$attachment_id = $this->import_attachment( $event_data['attachment'] );
+				if ( $attachment_id ) {
+
+					set_post_thumbnail( $post_id, $attachment_id );
+				}
+			}
+		}
+
 		return $post_id;
+	}
+
+	/**
+	 * Stores local copy of remote attachment
+	 *
+	 * @param array $attachment  {
+	 * 		Attachment data
+	 * 		@type string $url Remote file url
+	 * 		@type string $mime File MIME type
+	 * }
+	 * @return int Attachment ID
+	 */
+	private function import_attachment( $attachment ) {
+
+		$file = wp_remote_get( $attachment['url'] );
+		if( ! is_wp_error( $file ) ) {
+
+			$content = wp_remote_retrieve_body( $file );
+			$tmp_file_name = wp_tempnam();
+			file_put_contents( $tmp_file_name, $content );
+
+			$url = wp_parse_url( $attachment['url'] );
+			$attachment_file = array(
+				'name' 		=> wp_basename( $url['path'] ),
+				'type' 		=> $attachment['mime'],
+				'size' 		=> filesize( $tmp_file_name ),
+				'tmp_name' 	=> $tmp_file_name
+			);
+
+			$data = wp_handle_sideload( $attachment_file, array( 'test_form' => false ) );
+			if ( ! isset( $data['error'] ) ) {
+
+				$attachment = wp_insert_attachment(
+					array(
+						'guid'			  => $data['url'],
+						'post_title'	  => $attachment_file['name'],
+						'post_mime_type'  => $attachment['mime']
+					),
+					$data['file']
+				);
+				$image_meta_data = wp_generate_attachment_metadata( $attachment, $data['file'] );
+
+				if( $image_meta_data ) {
+					wp_update_attachment_metadata( $attachment, $data );
+				}
+				return $attachment;
+			}
+		}
+
+		return 0;
 	}
 
 	/**
@@ -192,10 +256,10 @@ class Calendar_Plus_iCal_Sync {
 		}
 
 		$location_id = wp_insert_post( [
-			'post_title'  => $location_name,
-			'post_status' => $post_status ? $post_status : $this->default_status,
-			'post_type'   => 'calendar_location',
-			'post_author' => $this->event_author,
+            'post_title'  => $location_name,
+            'post_status' => $post_status ? $post_status : $this->default_status,
+            'post_type'   => 'calendar_location',
+            'post_author' => $this->event_author,
 		] );
 
 		if ( is_wp_error( $location_id ) ) {
