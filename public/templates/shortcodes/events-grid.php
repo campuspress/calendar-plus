e4cd9ff34e315b0aff8b0dd6259ce589894fc595
AndreyIslentiev <andrey.islentiev@incsub.com>
2022-05-09 15:00:20 +1000

    Grid layout modifications

--- public/templates/shortcodes/events-grid.php
+++ public/templates/shortcodes/events-grid.php
@@ -2,35 +2,35 @@
 /**
  * @var array $event_groups
  * @var array $template_data {
-        @param int column_size
- *      @param int columns
+		@param int $column_size
+ *      @param int $columns
  * }
  */
 $is_inline = false;
 
 foreach ( $event_groups as $events_by_date ) {
-    $items_in_row = 0;
+	$items_in_row = 0;
 
 	foreach ( $events_by_date as $date => $events ) {
 		$month_name = mysql2date( 'M', $date, true );
 		$day        = mysql2date( 'd', $date, true );
 
 		?>
-        <?php if( $items_in_row === 0 ) {
-            ?>
-            <div class="calendarp-grid-row columns">
-            <?php
-        }
-        $items_in_row++;
-        ?>
-                <div class="calendarp large-<?php echo $template_data['column_size']; ?>">
-                    <?php include( calendarp_locate_template( 'shortcodes/events-list-item.php' ) ); ?>
-                </div>
-        <?php if( $items_in_row === $template_data['columns'] ) {
-            ?>
-            </div>
-            <?php
-            $items_in_row = 0;
-        }
-    }
+		<?php if( $items_in_row === 0 ) {
+			?>
+			<div class="calendarp-grid-row columns">
+			<?php
+		}
+		$items_in_row++;
+		?>
+				<div class="calendarp column large-<?php echo $template_data['column_size']; ?>">
+					<?php include( calendarp_locate_template( 'shortcodes/events-list-item.php' ) ); ?>
+				</div>
+		<?php if( $items_in_row === $template_data['columns'] ) {
+			?>
+			</div>
+			<?php
+			$items_in_row = 0;
+		}
+	}
 }
